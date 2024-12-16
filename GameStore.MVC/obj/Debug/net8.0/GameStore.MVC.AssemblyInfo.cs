57c4bedfa76b6c3d73f629fb0bdb3601fdfe8ad8
FizaretOsmanov <fizaretgithub@gmail.com>
2024-12-16 12:06:31 +0400

    .

--- GameStore.MVC/obj/Debug/net8.0/GameStore.MVC.AssemblyInfo.cs
+++ GameStore.MVC/obj/Debug/net8.0/GameStore.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985debb956927d717f4d2c66439fdc410c9a8bff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5db2e850db8b71f755fb02bb53b5daf960843f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
